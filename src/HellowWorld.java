d94d41ee7fcceaaf398166ce85b27a8f348515ad
whh <kennys57@163.com>
2024-04-25 15:10:42 +0800

    更改

---
+++ src/HellowWorld.java
@@ -0,0 +1,7 @@
+public class HellowWorld {
+    public static void main(String[] args) {
+        System.out.println("班级 02");
+        System.out.println("姓名 whh");
+        System.out.println("学号 2022133323");
+    }
+}
